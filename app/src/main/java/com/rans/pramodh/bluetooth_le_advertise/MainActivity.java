d7a3b6e39a196c97adb7b632cb7b25ae2762fc6e
Unknown <pramodhrachuri@gmail.com>
2018-04-11 00:50:46 +0530

    Initial

---
+++ app/src/main/java/com/rans/pramodh/bluetooth_le_advertise/MainActivity.java
@@ -0,0 +1,97 @@
+package com.rans.pramodh.bluetooth_le_advertise;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.os.ParcelUuid;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.charset.Charset;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mText;
+    private Button mAdvertiseButton;
+    private Button mDiscoverButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mText = (TextView) findViewById( R.id.text );
+        mDiscoverButton = (Button) findViewById( R.id.discover_btn );
+        mAdvertiseButton = (Button) findViewById( R.id.advertise_btn );
+
+        mDiscoverButton.setOnClickListener( this );
+        mAdvertiseButton.setOnClickListener( this );
+
+        if( !BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported() ) {
+            Toast.makeText( this, "Multiple advertisement not supported", Toast.LENGTH_SHORT ).show();
+            mAdvertiseButton.setEnabled( false );
+            mDiscoverButton.setEnabled( false );
+        }
+
+        String macAddress = android.provider.Settings.Secure.getString(this.getContentResolver(), "bluetooth_address");
+
+        Toast.makeText(this, macAddress, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if( v.getId() == R.id.discover_btn ) {
+            discover();
+        } else if( v.getId() == R.id.advertise_btn ) {
+            advertise();
+        }
+    }
+
+    public void discover()
+    {
+        Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
+    }
+
+    public void advertise()
+    {
+        BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode( AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY )
+                .setTxPowerLevel( AdvertiseSettings.ADVERTISE_TX_POWER_HIGH )
+                .setConnectable( false )
+                .build();
+
+        ParcelUuid pUuid = new ParcelUuid( UUID.fromString("d9a5e326-0049-4816-9851-70d1f52c4973") );
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName( false )
+                .addServiceUuid( pUuid )
+                .addServiceData( pUuid, "Data".getBytes( Charset.forName( "UTF-8" ) ) )
+                .build();
+
+        AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                Log.v("BLE","Advertising Started");
+                super.onStartSuccess(settingsInEffect);
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e( "BLE", "Advertising onStartFailure: " + errorCode );
+                super.onStartFailure(errorCode);
+            }
+        };
+
+        advertiser.startAdvertising( settings, data, advertisingCallback );
+    }
+}
